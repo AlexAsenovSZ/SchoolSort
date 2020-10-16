da1412028c1f204910d77a8dd22b45e7d84b6ea3
Alex Asenov <aleksandar.asenov@zaimov-pl.com>
2020-10-16 21:10:32 +0300

    file read to line

--- SchoolSum/SchoolSum/Program.cs
+++ SchoolSum/SchoolSum/Program.cs
@@ -6,9 +6,13 @@ namespace SchoolSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string text = System.IO.File.ReadAllText(@"C:\Users\AlexAsenov\source\repos\SchoolSum\files\class-demo.csv");
-            Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            Console.WriteLine("Hello SchoolSort!");
+            //string text = System.IO.File.ReadAllText(@"C:\Users\AlexAsenov\source\repos\SchoolSum\files\class-demo.csv");
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\AlexAsenov\source\repos\SchoolSum\files\class-demo.csv");
+            foreach (string line in lines)
+            { 
+                Console.WriteLine("\t" + line);
+            }
         }
     }
 }
