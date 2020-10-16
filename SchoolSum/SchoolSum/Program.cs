f15f4c86fa8a8a82eb6505caf9168b9ebc1eda7e
Alex Asenov <aleksandar.asenov@zaimov-pl.com>
2020-10-16 20:57:49 +0300

    file read to string

---
+++ SchoolSum/SchoolSum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SchoolSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string text = System.IO.File.ReadAllText(@"C:\Users\AlexAsenov\source\repos\SchoolSum\files\class-demo.csv");
+            Console.WriteLine("Contents of WriteText.txt = {0}", text);
+        }
+    }
+}
